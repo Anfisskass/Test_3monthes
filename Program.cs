c3c0f68fa73657497f99b0ae906d199011649ca4
anfisskass <nasyazh@mail.ru>
2022-07-28 17:44:43 +0300

    New array

--- Program.cs
+++ Program.cs
@@ -1,1 +1,16 @@
 ﻿
+
+int[] CreateRandomArray(int size, int min, int max)
+{
+    int[] newArray = new int [size];
+    
+    for (int i = 0; i < size; i++)
+    {
+        newArray[i] = new Random().Next(min, max +1);
+
+        Console.Write(newArray[i] + "  ");
+    }
+    Console.WriteLine();
+
+    return newArray;
+}
