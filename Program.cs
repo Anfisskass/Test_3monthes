c5f21f1372808143a63c9ed885137687894fe998
anfisskass <nasyazh@mail.ru>
2022-07-28 23:59:50 +0300

    The solution of test

--- Program.cs
+++ Program.cs
@@ -1,27 +1,44 @@
-﻿
-string[] CreateStringArray(int size)
+﻿string[] CreateStringArray(int size)
 {
     string[] words = new string[size];
     for(int i = 0; i < size; i++)
     {
-        Console.Write($"Input {i + 1} word ");
+        Console.WriteLine($"Input {i + 1} word ");
         words[i] = Console.ReadLine();
     }
     return words;
 }
 
-void Threelongsymbol(string[] array, string[] array2)
-{
-    string[] array = new string[size];
-    string[] array2 = new string[array1.Length];
-    int count = 0;
 
+void NumberOfLongWords(string[] array, string[] array2)
+{   
+    int count = 0;
     for (int i = 0; i < array.Length; i++)
     {
-    if(array[i].Length <= 3)
+        if(array[i].Length <= 3)
         {
-        array2[count] = array[i];
-        count++;
+            array2[count] = array[i];
+            count++;
         }
-    }
+    
+    }   
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.WriteLine($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+Console.Write("Input number of names: ");
+int size = Convert.ToInt32(Console.ReadLine());
+
+string[] array = CreateStringArray(size);
+string[] array2 = new string[array.Length];
+
+NumberOfLongWords(array, array2);
+Console.WriteLine();
+PrintArray(array2); 
